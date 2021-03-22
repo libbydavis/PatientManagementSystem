8c77c7b1482f50a6cf42f9dec7ce3bff1f5f6ec1
libby <libstarnz@gmail.com>
2021-03-22 18:52:28 +1300

    testing

---
+++ src/ProgramFilesPackage/Dosage.java
@@ -0,0 +1,12 @@
+package ProgramFilesPackage;
+
+public class Dosage {
+    double amount;
+    String howOften;
+    String test;
+    String thisisdumbv;
+    String test2;
+    String test3;
+    String test43;
+    String plswork;
+}
