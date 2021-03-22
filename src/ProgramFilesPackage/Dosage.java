d292fd792f10238a634b1801c81480bcd5f90916
rajrathod619 <rajrathod619@gmail.com>
2021-03-22 20:02:54 +1300

    eclipse test

--- src/ProgramFilesPackage/Dosage.java
+++ src/ProgramFilesPackage/Dosage.java
@@ -4,9 +4,4 @@ public class Dosage {
     double amount;
     String howOften;
     String test;
-    String thisisdumbv;
-    String test2;
-    String test3;
-    String test43;
-    String plswork;
 }
