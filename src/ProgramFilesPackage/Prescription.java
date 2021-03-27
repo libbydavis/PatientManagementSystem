898b66d0cdba81d5d4f14ec468aa100908567ebb
rajrathod619 <rajrathod619@gmail.com>
2021-03-27 16:44:22 +1300

    Added constructor - will probably need to change for date, time
    - INCOMEPLETE

--- src/ProgramFilesPackage/Prescription.java
+++ src/ProgramFilesPackage/Prescription.java
@@ -7,4 +7,14 @@ public class Prescription {
 	String doctorName;
 	Patient patientDetails;
 	Boolean repeat;
+	
+	public Prescription(String date, String time, Medication meds, String doctorName, Patient patientDetails, Boolean repeat) 
+	{	
+		this.date = date;
+		this.time = time;
+		this.meds = meds;
+		this.doctorName = doctorName;
+		this.patientDetails = patientDetails;
+		this.repeat = repeat;
+	}
 }
