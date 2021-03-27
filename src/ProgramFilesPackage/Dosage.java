a2939bef8113340724bccdd52944deca0d4c4464
rajrathod619 <rajrathod619@gmail.com>
2021-03-27 16:40:51 +1300

    Added constructor, added toString

--- src/ProgramFilesPackage/Dosage.java
+++ src/ProgramFilesPackage/Dosage.java
@@ -1,6 +1,18 @@
 package ProgramFilesPackage;
 
 public class Dosage {
-    double amount;
+    
+	double amount;
     String howOften;
+	
+    public Dosage(double amount, String howOften) 
+    {
+		this.amount = amount;
+		this.howOften = howOften;
+	}
+
+	public String toString() 
+	{
+		return "Dosage amount: " +amount + ", Frequency: " + howOften;
+	}   
 }
